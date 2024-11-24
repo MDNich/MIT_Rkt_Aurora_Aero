e7df02b64a3a89e5ffe7238db4c3e7f222779ef7
MDNich <71262643+MDNich@users.noreply.github.com>
2024-11-24 17:04:34 -0500

    FEA complete.

---
+++ finFEA/wspc_FEA_files/dp0/ACP-Pre/ACP/runACP.py
@@ -0,0 +1,4 @@
+﻿# -*- coding: utf-8 -*-
+import compolyx
+db = compolyx.DB()
+db.open( path=u"Z:\\Developer\\MIT_Rkt_Team\\MIT_Rkt_Aurora_Aero\\MIT_Rkt_Aurora_Aero\\finFEA\\wspc_FEA_files\\dp0\\ACP-Pre\\ACP\\ACP-Pre.acph5" )
